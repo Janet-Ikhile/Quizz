9b757074230a60baf203d45d03ea21dbf75cb4e6
Janet-Ikhile <adazejohnson@gmail.com>
2021-08-14 22:14:03 +0100

    A test class has been created. Woah!!!

---
+++ src/test/java/com/adaze/Quizz/controller/QuizzControllerTest.java
@@ -0,0 +1,13 @@
+package com.adaze.Quizz.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizzControllerTest {
+
+    @Test
+    void tellThem() {
+        assertEquals(2,1+1);
+    }
+}
