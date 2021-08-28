013e8a5cf4fce5472f86e5d59e3ff7de7b87b249
Janet-Ikhile <adazejohnson@gmail.com>
2021-08-28 23:41:34 +0100

    Add more questions to the Quizz Service

--- src/main/java/com/adaze/quizz/service/QuizzService.java
+++ src/main/java/com/adaze/quizz/service/QuizzService.java
@@ -15,7 +15,10 @@ public class QuizzService {
             new Question(1, "Who was the first man God created?", "Adam", Arrays.asList("Gabriel", "John", "Adam", "Phillips")),
             new Question(2, "Who was the vilian in Avengers Endgame", "Thanos", Arrays.asList("Black Widow", "Spider Man", "Thor", "Thanos")),
             new Question(3, "Who sang running to you ", "Simi&Chike", Arrays.asList("Mark Angel", "Timaya", "Simi&Chike", "Davido")),
-            new Question(4, "How many sides does a dice have", "6", Arrays.asList("90", "87", "1", "6"))
+            new Question(4, "How many sides does a dice have", "6", Arrays.asList("90", "87", "1", "6")),
+            new Question(5, "The only fish that can be found on a crayon is called", "Crayfish", Arrays.asList("Tilapia", "Titus", "Fish", "Crayfish")),
+            new Question(6, "Who is the Brain Box of Seamfix", "Chimezie", Arrays.asList("Chibuzor", "Tochukwu", "Chimezie", "Olamide"))
+
     ));
 
     public Question randomQuestion() {
@@ -31,7 +34,7 @@ public class QuizzService {
     }
 
     public Question getSelectedQuestion(int id) {
-        Question question = questions.get(id-1);
+        Question question = questions.get(id - 1);
         return question;
     }
 
